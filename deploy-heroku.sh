61030c9d6e5aec2ffa6b89ca5aec5162e3661813
Konrad C <conrisc@gmail.com>
2019-12-05 10:16:06 +0100

    Adds deploy-heroku.sh

---
+++ deploy-heroku.sh
@@ -0,0 +1,9 @@
+git checkout -b temp-heroku-master
+rm .gitignore
+echo /javascript > .gitignore
+mv nodejs-server/* ./
+git add .
+git commit -m 'Heroku nodejs-server'
+git push -f heroku temp-heroku-master:master
+git checkout master
+git branch -D temp-heroku-master
