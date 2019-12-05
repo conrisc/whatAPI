d5b4870f26af8b9f46d7a0d334ad083592096755
Konrad C <conrisc@gmail.com>
2019-12-05 10:41:36 +0100

    Put credentials to ignore/ directory

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,7 +1,9 @@
 git checkout -b temp-heroku-master
 rm .gitignore
 echo /javascript > .gitignore
+echo /ignore >> .gitignore
 mv nodejs-server/* ./
+cp ignore/credentials.js ./
 git add .
 git commit -m 'Heroku nodejs-server'
 git push -f heroku temp-heroku-master:master
