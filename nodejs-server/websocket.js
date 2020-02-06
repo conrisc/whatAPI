a86f6a0757af12fe636fbc6332eeaa54c367d0d6
Konrad C <conrisc@gmail.com>
2020-02-06 19:42:40 +0100

    Changes websocket to use native ping method

--- nodejs-server/websocket.js
+++ nodejs-server/websocket.js
@@ -9,11 +9,11 @@ const dataTypes = {
     VOLUME_DOWN: 'volume_down',
     LOAD_VIDEO: 'load_video',
     PING: 'ping',
-    PONG: 'pong'
+    PONG: 'pong',
+    CLIENTS_INFO: 'clients_info'
 }
 
-const clients = [];
-
+let clients = [];
 
 function injectWebSocket(server) {
     const wss = new WebSocket.Server({ server });
@@ -24,16 +24,18 @@ function injectWebSocket(server) {
 
 function injectConfiguration(wss) {
 
-    const sendMessage = (json, ws) => {
+    const sendMessage = (msg, ws) => {
         clients.forEach((client) => {
             if (client != ws) {
-                client.send(json)
+                client.send(msg)
             }
         });
     }
 
     const addClientToChat = (dataFromClient, ws) => {
+        ws.name = randName();
         clients.push(ws);
+        ws.send(JSON.stringify({ type: dataTypes.JOIN, name: ws.name }));
     }
 
     const handleNewMessage = (dataFromClient, ws) => {
@@ -44,25 +46,17 @@ function injectConfiguration(wss) {
         sendMessage(JSON.stringify(response), ws);
     }
 
-    const sendPong = (ws) => {
-        const response = {
-            type: dataTypes.PONG
-        }
-        ws.send(JSON.stringify(response));
-    }
-
-    wss.on('connection', function (ws) {
-        console.log('new connection');
+    wss.on('connection', function (ws, request, client) {
+        ws.isAlive = true;
+        console.log('New connection for user: ', request.connection);
+        console.log('With headers:', request.headers);
 
         ws.on('message', function(message) {
-            console.log(message);
+            console.log('ws <message>: ', message);
             const dataFromClient = JSON.parse(message);
             switch(dataFromClient.type) {
                 case dataTypes.JOIN:
                     addClientToChat(dataFromClient, ws);
-                    break;
-                case dataTypes.PING:
-                    sendPong(ws);
                     break;
                 case dataTypes.NEW_MESSAGE:
                 case dataTypes.PLAY:
@@ -71,12 +65,46 @@ function injectConfiguration(wss) {
                 case dataTypes.VOLUME_DOWN:
                 case dataTypes.LOAD_VIDEO:
                 default:
-                    handleNewMessage(dataFromClient, ws)
+                    handleNewMessage(dataFromClient, ws);
             }
         });
+
+        ws.on(dataTypes.PONG, heartbeat);
     });
+
+    function heartbeat() {
+        console.log('Got pong from:', this.name);
+        this.isAlive = true;
+    }
+
+    setInterval(() => {
+        clients = clients.filter(ws => {
+            if (!ws.isAlive)
+                ws.terminate();
+            return ws.isAlive;
+        });
+        const data = {
+            type: dataTypes.CLIENTS_INFO,
+            clients: clients.map(ws => ws.name)
+        };
+        clients.forEach(ws => {
+            ws.isAlive = false;
+            ws.ping();
+            sendClientsInfo(data, ws);
+        });
+    }, 30000);
+}
+
+function sendClientsInfo(data, ws) {
+    ws.send(JSON.stringify(data));
+}
+
+function randName() {
+    const length = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-0123456789';
+    return Array(length).fill('').map(() => characters[Math.floor(Math.random() * characters.length)]).join('');
 }
 
 module.exports = {
     injectWebSocket
-}
+};
